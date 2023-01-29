821a9d222c29c54ba1202c25e4cd4b32ea7e9e60
arise <arisa8533@gmail.com>
2023-01-29 05:58:47 +0000

    Quiz 5 files

--- src/components/Quiz1.jsx
+++ src/components/Quiz1.jsx
@@ -1,5 +1,6 @@
 const Quiz1 = () => {
-    return (
-        <p>Quiz 1 done</p>
-    )
-}
+  return <p>Quiz 1 done</p>;
+};
+
+// export {Quiz1} // export many function
+export default Quiz1; // only one function to use for export มีแค่ 1 หน้าที่การทำงาน
--- src/components/Quiz4.jsx
+++ src/components/Quiz4.jsx
@@ -1,1 +1,38 @@
 // Write component name Quiz4 and display your name
+
+const Quiz4 = () => {
+  const book = {
+    title: "Arisa A.",
+    price: 10,
+  };
+
+  return <Child3 book={book} />;
+  //   return <Child2 text="Arisa A." price={10} />;
+  //   return <Child text="Arisa A." price={10} />;
+};
+
+function Child(props) {
+  return (
+    <div>
+      {props.text} ${props.price} included VAT ${props.price + 1}
+    </div>
+  );
+}
+
+function Child2({ text, price }) {
+  return (
+    <div>
+      {text} ${price} included VAT ${price + 1}
+    </div>
+  );
+}
+
+function Child3({ book }) {
+  return (
+    <div>
+      {book.title} ${book.price} included VAT ${book.price + 1}
+    </div>
+  );
+}
+
+export default Quiz4;
